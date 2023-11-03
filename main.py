d7554f43c30fcf237a290194a9b456395172b125
CodeGeek2003 <109857503+CodeGeek2003@users.noreply.github.com>
2023-11-03 18:43:47 +0200

    update commit

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from tkinter import messagebox
 
 otp = random.randint(100000, 999999)
 otp_str = str(otp)
-previous_email = ""  # Store the previous email
-
+previous_email = ""
 def send_email(receiver_email, otp):
-    sender_email = "Enter Your EMail"  # Your email address
-    sender_password = "Enter Your Password"  # Your email password
+    sender_email = "Enter Your EMail"
+    sender_password = "Enter Your Password"
     message = "Subject: OTP Verification\n\nYour OTP is " + otp
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -24,7 +23,6 @@ def send_email(receiver_email, otp):
     except Exception as e:
         print("Error occurred: ", e)
         return False
-
 def resend_otp():
     global otp_str
     if previous_email:
@@ -34,7 +32,6 @@ def resend_otp():
             messagebox.showerror("Error", "Unable to resend OTP. Please check your email settings.")
     else:
         messagebox.showerror("Error", "No previous email to resend OTP.")
-
 def verify_otp(otp_entry, otp_window):
     user_otp = otp_entry.get()
     if user_otp == otp_str:
@@ -43,20 +40,17 @@ def verify_otp(otp_entry, otp_window):
         show_final_message()
     else:
         messagebox.showerror("Error", "OTP Verification failed. Please try again.")
-
 def restart_app():
     global otp, otp_str
     otp = random.randint(100000, 999999)
     otp_str = str(otp)
     final_message.destroy()
-    email_window.deiconify()  # Show the email entry window again
-    email_entry.delete(0, 'end')  # Clear the previous email
-    otp_entry.delete(0, 'end')  # Clear the previous OTP
-
+    email_window.deiconify()
+    email_entry.delete(0, 'end')
+    otp_entry.delete(0, 'end')
 def close_app():
     final_message.destroy()
-    email_window.destroy()  # Close the entire app
-
+    email_window.destroy()
 def show_final_message():
     global final_message
     final_message = tk.Tk()
@@ -68,15 +62,13 @@ def show_final_message():
     send_another_button.pack()
     exit_button.pack()
     final_message.mainloop()
-
 def send_email_and_open_otp_window():
     global previous_email
     user_email = email_entry.get()
     previous_email = user_email
     if send_email(user_email, otp_str):
         messagebox.showinfo("Success", "OTP sent successfully!")
-        email_window.withdraw()  # Hide the email entry window
-
+        email_window.withdraw()
         otp_window = tk.Tk()
         otp_window.title("Enter OTP")
         otp_label = tk.Label(otp_window, text="Enter the OTP you received:")
@@ -89,8 +81,6 @@ def send_email_and_open_otp_window():
         resend_otp_button = tk.Button(otp_window, text="Resend OTP", command=resend_otp)
         resend_otp_button.pack()
         otp_window.mainloop()
-
-# Create the main email entry window
 email_window = tk.Tk()
 email_window.title("Email Entry")
 email_label = tk.Label(email_window, text="Enter your email address:")
@@ -99,5 +89,4 @@ email_entry = tk.Entry(email_window)
 email_entry.pack()
 send_button = tk.Button(email_window, text="Send", command=send_email_and_open_otp_window)
 send_button.pack()
-
 email_window.mainloop()
